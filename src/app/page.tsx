929a089b27cc9406b38dfa83aacf5aa60ed2216e
leejihoon <ljhljh211@naver.com>
2024-05-12 23:51:16 +0900

    style: tailwind config 컬러 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,73 @@
 'use client'
 
-import { css } from 'twin.macro'
-
-import { Button } from '@/components/Button'
+import { css, styled, theme } from 'twin.macro'
 
 const MainPage = () => {
   return (
-    <div
-      css={css`
-        border: 1px solid red;
-      `}
-    >
-      메인
-      <Button>버튼</Button>
+    <div>
+      <Header>
+        {/* 로고 */}
+        <Inner>
+          <LogoWrapper>로고 영역</LogoWrapper>
+          <Menu>
+            <MenuItem>메뉴1</MenuItem>
+            <MenuItem>메뉴2</MenuItem>
+          </Menu>
+        </Inner>
+      </Header>
+      <section tw="max-w-[1200px] mx-[auto]">
+        <div tw="grid grid-cols-4">
+          <Box tw="bg-primary-40" />
+          <Box tw="bg-secondary-40" />
+          <Box tw="bg-primary-90" />
+          <Box tw="bg-tertiary-90" />
+        </div>
+      </section>
     </div>
   )
 }
 
 export default MainPage
+
+const LogoWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 120px;
+  height: 30px;
+  background-color: ${theme`colors.neutral-variant.90`};
+`
+
+const Header = styled.header`
+  border-bottom: 1px solid ${theme`colors.schemes.light.outlineVariant`};
+`
+
+const Inner = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  /* gutter */
+  padding: 0 30px;
+  height: 50px;
+  max-width: 1200px;
+  margin: 0 auto;
+`
+
+const Menu = styled.ul`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  li {
+  }
+`
+
+const MenuItem = styled.li`
+  /* color: ${theme`colors.primary`}; */
+  font-weight: 600;
+  cursor: pointer;
+`
+
+const Box = styled.div`
+  min-width: 20px;
+  height: 500px;
+`
--- src/styles/GlobalStyles.tsx
+++ src/styles/GlobalStyles.tsx
@@ -1,12 +1,13 @@
 'use client'
 
-import React from 'react'
 import { Global } from '@emotion/react'
-import tw, { css, theme, GlobalStyles as BaseStyles } from 'twin.macro'
+import React from 'react'
+import tw, { GlobalStyles as BaseStyles, css, theme } from 'twin.macro'
 
 const customStyles = css({
   body: {
-    WebkitTapHighlightColor: theme`colors.purple.500`,
+    backgroundColor: theme`colors.schemes.light.background`,
+    WebkitTapHighlightColor: theme`colors.schemes.light.scrim`,
     ...tw`antialiased`,
   },
 })
