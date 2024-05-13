3ea05150e00d5afa7797d7c375e1b238b2a5f74b
leejihoon <ljhljh211@naver.com>
2024-05-13 22:45:27 +0900

    feat: 임시 섹션 영역 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,18 +2,30 @@
 
 import { styled } from 'twin.macro'
 
+import Area from '@/components/layouts/Area'
+
 const MainPage = () => {
   return (
     <div>
-      <section tw="max-w-[1200px] mx-[auto]">
-        <div tw="grid grid-cols-4">
-          <Box tw="bg-primary-40" />
-          <Box tw="bg-secondary-40" />
-          <Box tw="bg-primary-90" />
-          <Box tw="bg-tertiary-90" />
-        </div>
+      <section>
+        <Area>
+          <div tw="grid grid-cols-4">
+            <Box tw="bg-primary-40" />
+            <Box tw="bg-secondary-40" />
+            <Box tw="bg-primary-90" />
+            <Box tw="bg-tertiary-90" />
+          </div>
+        </Area>
       </section>
-      <section tw="h-[100vh]">2번째</section>
+      <section tw="h-[100vh] bg-schemes-light-inversePrimary flex items-center justify-center text-center">
+        <Area>2번째 섹션</Area>
+      </section>
+      <section tw="h-[100vh] bg-schemes-light-medium-contrast-surfaceContainerHighest flex items-center justify-center text-center">
+        <Area>3번째 섹션</Area>
+      </section>
+      <section tw="h-[100vh] bg-schemes-light-secondaryFixed flex items-center justify-center text-center">
+        <Area>4번째 섹션</Area>
+      </section>
     </div>
   )
 }
